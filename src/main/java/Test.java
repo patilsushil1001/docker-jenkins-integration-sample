e3a0b374814951c01e604c41af4777cd2a19ebd7
patilsushil1001 <patilsushil1001@gmail.com>
2020-06-14 01:48:32 +0530

    4th commit

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,5 +2,6 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println("Hello Sushil here, this is Docker, Jenkins and spring boot CICD.");
+        System.out.println("Commited from Local for Jenkins.");
     }
 }
